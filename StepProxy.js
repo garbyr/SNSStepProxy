b6a8a24f5f8b9bd653db4e7961c3385ee109aeea
GaryB <garbyr@gmail.com>
2017-08-19 21:11:51 +0100

    bugzzzz

--- StepProxy.js
+++ StepProxy.js
@@ -1,17 +1,17 @@
-let AWS = require('aws-sdk');  
-let stepfunctions = new AWS.StepFunctions();
+const aws = require('aws-sdk');  
+var stepfunctions = new aws.StepFunctions();
 
-module.exports.invoke = function(event, context, callback) {  
-  let accountId = context.invokedFunctionArn.split(':')[4];
+exports.handler = (event, context, callback) => { 
+  var accountId = context.invokedFunctionArn.split(':')[4];
   var messageObj = event.Records[0].Sns.Message;
   var message = JSON.parse(messageObj);
 
   //hardcode statemachine for now, should be parameter passed in via SNS
   let params = {
-    stateMachineArn: `arn:aws:states:us-east-1:${accountId}:stateMachine:`+NAVCSVProcessV0_1,
-    input: event.message,
+    stateMachineArn: `arn:aws:states:us-east-1:${accountId}:stateMachine:`+'NAVCSVProcessV0_1',
+    input: message,
   };
-  console.log("params");
+  console.log(params);
 
   //stepfunctions.startExecution(params, callback);
-};
+}
---
+++ test.js
@@ -0,0 +1,36 @@
+// Our Lambda function fle is required 
+var index = require('./StepProxy.js');
+
+// The Lambda context "done" function is called when complete with/without error
+var context = {
+    done: function (err, result) {
+        console.log('------------');
+        console.log('Context done');
+        console.log('   error:', err);
+        console.log('   result:', result);
+    }
+};
+
+// Simulated S3 bucket event
+var event = {
+    Records: [
+        {
+            Sns: {
+                Message: {
+                    calculateSRRI: 'Yes',
+                    requestUUID: 'abc123',
+                    ISIN: 'x12345',
+                    NAV: 0.123,
+                    frequency: 'Weekly',
+                    category: 'Market',
+                    user: 'Gary',
+                    calculationDate: '21/07/2017',
+                    description: 'ICIN x12345'
+                }
+            }
+        }
+    ]
+};
+
+// Call the Lambda function
+index.handler(event, context);
